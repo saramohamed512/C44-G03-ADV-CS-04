a2ef806148db93b1ef55db0ae4b2d1346495cf1d
saramohamed512 <saramohamed12656@gmail.com>
2025-08-22 17:47:18 +0200

    Delegate & Delegate Example 01

--- AdvancedSession4/Program.cs
+++ AdvancedSession4/Program.cs
@@ -6,7 +6,26 @@ namespace AdvancedSession4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region Delegate 
+            #region Delegate Example01
+            //int x;
+            //x=5;
+            //x=6;
+            //step01 Declare refrence from delegate
+            StringFunctionDelegate reference;
+            //step02 intialize reference to function 
+            //reference = new StringFunctionDelegate(StringFunction.CountOfUpperCase);
+            reference = StringFunction.CountOfUpperCase;// Syntax Sugar
+            //reference += StringFunction.CountOfLowerCase;//add method to delegate
+            //reference -= StringFunction.CountOfLowerCase;//remove method to delegate
+            //step03 use delegate
+            int Result = reference.Invoke("SarA");
+            Console.WriteLine(Result);
+
+
+            #endregion
+
+            #endregion
         }
     }
 }
